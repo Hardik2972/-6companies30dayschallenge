8a63bce16b1a6012487db5fae2d391b26b78bcf4
Hardik2972 <garghardik2972@gmail.com>
2024-01-15 00:24:38 +0530

    Day 14U

--- Maximum_Length_of_Repeated_Subarray.cpp
+++ Maximum_Length_of_Repeated_Subarray.cpp
@@ -6,21 +6,30 @@
 using namespace std;
 class Solution {
 public:
-    int recursion(int i,int j,vector<int>& nums1,vector<int>& nums2,vector<vector<int>>& dp){
-        if(i==nums1.size() || j==nums2.size()) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int first = recursion(i+1,j,nums1,nums2,dp);
-        int second = recursion(i,j+1,nums1,nums2,dp);
-        int third = recursion(i+1,j+1,nums1,nums2,dp);
-        if(nums1[i]==nums2[j]) cout<<i<<"  "<<j<<endl;
-        int add = 0;
-        if(nums1[i]==nums2[j] && (i+1==nums1.size() || j+1==nums2.size() || nums1[i+1]==nums2[j+1])){
-            add = 1;
-        }
-        return dp[i][j]=max(first,max(second,third+add));
-    }
+    
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size(),vector<int>(nums2.size(),-1));
-        return recursion(0,0,nums1,nums2,dp);
+        vector<vector<int>> dp(nums1.size(),vector<int>(nums2.size(),0));
+        int ans = 0;
+        for(int i=0;i<nums2.size();i++){
+            dp[0][i]=(nums1[0]==nums2[i]);
+            ans=max(ans,dp[0][i]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            dp[i][0]=(nums1[i]==nums2[0]);
+            ans=max(ans,dp[i][0]);
+        }
+        
+        for(int i=1;i<nums1.size();i++){
+            for(int j=1;j<nums2.size();j++){
+                if(nums1[i]!=nums2[j]){
+                    dp[i][j]=0;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                ans=max(ans,dp[i][j]);
+            }
+        }
+        return ans;
     }
 };
